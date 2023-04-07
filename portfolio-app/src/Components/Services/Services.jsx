b9d78a74a0cbe19beeedcff76e079ae5ad4ace8e
Swati <swati829955@gmail.com>
2023-04-07 20:01:34 +0530

    projects

--- portfolio-app/src/Components/Services/Services.jsx
+++ portfolio-app/src/Components/Services/Services.jsx
@@ -4,6 +4,7 @@ import HeartEmoji from "../../img/heartemoji.png";
 import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import Card from '../Card/Card';
+import Resume from './Resume.pdf';
 const Services = () => {
   return (
     <div className="services">
@@ -11,20 +12,43 @@ const Services = () => {
 <span>My Awesome </span>
 <span>Projects</span>
 <spane>Lorem, ipsum dolor sit amet consectetur <br /> adipisicing elit. Dolor, vero.</spane>
+<a href={Resume} download>
 <button className="button s-button"> Download CV</button>
+</a>
 <div className="blur s-blurl" style={{ background :"#ABF1FF94" }}></div>
       </div>
 
       <div className="cards">
       
-      <div>
+      <div style={{left : '14rem'}}>
         <Card
         emoji={HeartEmoji}
         heading = {'Design'}
         detail = {"Figma , Sketch"}
         />
       </div>
-
+      <div style={{left : '14rem'}}>
+        <Card
+        emoji={HeartEmoji}
+        heading = {'Design'}
+        detail = {"Figma , Sketch"}
+        />
+      </div>
+      <div style={{top:"12rem", left:"-4rem"}}>
+        <Card
+          emoji={Glasses}
+          heading={"Data Analysis"}
+          detail = {"python"}
+        />
+      </div>
+      <div style={{top:"19rem", left:"12rem"}}>
+        <Card
+          emoji={Humble}
+          heading={"Website"}
+          detail = {"HTML , CSS "}
+        />
+      </div>
+      <div className="blur s-blur" style={{background : "var(--purple)"}}></div>
       </div>
     </div>
   )
